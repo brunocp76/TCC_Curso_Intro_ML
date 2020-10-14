b6c1dcb002750e1c3f97e62cbe6af4a4e8d97a91
Bruno Cesar Pasquini <39478173+brunocp76@users.noreply.github.com>
2020-10-14 18:11:37 -0300

    Primeiras Análises

---
+++ R/0 - Analise Exploratoria de Dados.R
@@ -0,0 +1,110 @@
+# Bibliotecas para a Análise ----------------------------------------------
+library(SmartEDA)
+library(tidyverse)
+library(Information)
+library(DataExplorer)
+
+
+# Importando os dados... --------------------------------------------------
+adult_val <- readRDS(file = "data-raw/adult_val.rds")
+
+adult <- readRDS(file = "data-raw/adult.rds")
+
+
+# Primeiras Análises ------------------------------------------------------
+adult %>% skimr::skim()
+
+adult_val %>% glimpse()
+
+adult %>% glimpse()
+
+adult %>% count(resposta)
+
+
+# Análises Exploratórias --------------------------------------------------
+SmartEDA::ExpData(
+   data = adult,
+   type = 1
+)
+
+SmartEDA::ExpData(
+   data = adult,
+   type = 2
+)
+
+
+SmartEDA::ExpNumStat(
+   data = adult,
+   by = "GA",
+   gp = "resposta",
+   Qnt = NULL,
+   MesofShape = 2,
+   Outlier = TRUE,
+   round = 2
+)
+
+SmartEDA::ExpNumViz(
+   data = adult,
+   target = "resposta",
+   type = 1,
+   # nlim = NULL,
+   fname = NULL,
+   col = c("blue", "red"),
+   Page = c(3, 2),
+   sample = NULL
+)
+
+SmartEDA::ExpCTable(
+   data = adult,
+   Target = "resposta",
+   margin = 1,
+   clim = 15,
+   # nlim = NULL,
+   round = 2,
+   bin = NULL,
+   per = TRUE
+)
+
+SmartEDA::ExpCatStat(
+   data = adult,
+   Target = "resposta",
+   result = "IV",
+   clim = 20,
+   # nlim = NULL,
+   bins = 20,
+   plot = TRUE,
+   top = 30,
+   Round = 3
+)
+
+SmartEDA::ExpCatStat(
+   data = adult,
+   Target = "resposta",
+   result = "Stat",
+   clim = 20,
+   # nlim = NULL,
+   bins = 20,
+   plot = TRUE,
+   top = 30,
+   Round = 5
+) %>%
+   filter(Variable != "id") %>%
+   arrange(desc(`Cramers V`), desc(`Chi-squared`))
+
+
+adult %>%
+   mutate(
+      resp_num = ifelse(test = resposta == "<=50K",
+                        yes = 0L,
+                        no = 1L)
+   ) %>%
+   select(-id, -resposta) %>%
+   Information::create_infotables(
+      y = "resp_num",
+      bins = 20,
+      parallel = TRUE
+   )
+
+
+table(adult$sex, adult$resposta)
+
